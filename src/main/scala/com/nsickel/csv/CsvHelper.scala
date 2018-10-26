1d92061e5146f732b85e5640ea4c1e3bfe53aa69
Nils Sickel <nils.sickel@gmail.com>
2018-10-26 18:59:12 +0200

    add mode filter duplicates

--- src/main/scala/com/nsickel/csv/CsvHelper.scala
+++ src/main/scala/com/nsickel/csv/CsvHelper.scala
@@ -1,26 +1,17 @@
 package com.nsickel.csv
 
-import scala.collection.mutable.LinkedHashSet
-
 object CsvHelper extends App {
 
-  val mode = args(0)
+  var mode = args(0)
+  val csvReader = new CsvReader
 
-  def readFileLines(pathToFile:String) : LinkedHashSet[String] = {
-    val linesCsvFile = new LinkedHashSet[String]
-    val bufferedSource = io.Source.fromFile(pathToFile)
-    for (line <- bufferedSource.getLines) {
-      linesCsvFile.add(line)
-    }
-    bufferedSource.close
-    linesCsvFile
-  }
-
-  if (mode.equals("filter-unique")) {
-    val linesCsvFileOne = readFileLines(args(1))
-    val linesCsvFileTwo = readFileLines(args(2))
-
-    linesCsvFileTwo.foreach(line => if (!linesCsvFileOne.contains(line)) println(line))
+  mode match {
+    case CliCommands.ModeFilterUnique =>
+      csvReader.printUniqueLinesToComparedFile(args(1),args(2))
+    case CliCommands.ModeRemoveDuplicates =>
+      csvReader.printFileWithoutDuplicates(args(1))
+    case _ =>
+      CliCommands.printHelpText()
   }
 }
 
